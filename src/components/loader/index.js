b6007209601bdff02b68618fe48ffc261a01fa59
Sly Flores <sly@christian.com.ph>
2024-11-23 23:43:17 +0800

    ...

--- src/components/loader/index.js
+++ src/components/loader/index.js
@@ -1,2 +1,2 @@
-export { default as LoaderWithLogo } from './LoaderWithLogo';
 export { default as LoadingProgress } from './LoadingProgress';
+export { default as LoaderWithLogo } from './LoaderWithLogo';
