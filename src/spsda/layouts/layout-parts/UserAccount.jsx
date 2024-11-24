381459bc52fffd8ab58dc98d8eb9932f039c48b1
Sly Flores <sly@christian.com.ph>
2024-11-24 14:06:27 +0800

    ...

--- src/spsda/layouts/layout-parts/UserAccount.jsx
+++ src/spsda/layouts/layout-parts/UserAccount.jsx
@@ -1,22 +1,21 @@
 import Box from '@mui/material/Box';
 import Chip from '@mui/material/Chip';
-import Badge from '@mui/material/Badge';
-import Button from '@mui/material/Button'; // CUSTOM COMPONENTS
+// import Badge from '@mui/material/Badge';
+// import Button from '@mui/material/Button'; // CUSTOM COMPONENTS
 
 import { Paragraph } from '@/components/typography';
 import AvatarLoading from '@/components/avatar-loading';
 import FlexRowAlign from '@/components/flexbox/FlexRowAlign';
 export default function UserAccount() {
   return <FlexRowAlign flexDirection="column" py={5}>
-      <Badge badgeContent="Free" color="primary">
-        <AvatarLoading alt="user" percentage={60} src="/static/user/user-11.png" sx={{
+
+      <AvatarLoading alt="user" percentage={100} src="/static/user/user-11.png" sx={{
         width: 50,
         height: 50
       }} />
-      </Badge>
 
       <Box textAlign="center" pt={1.5} pb={3}>
-        <Chip variant="outlined" label="60% Complete" size="small" />
+        <Chip variant="outlined" label="Teacher" size="small" />
 
         <Paragraph fontSize={16} fontWeight={600} mt={2}>
           Aaron Cooper
@@ -27,6 +26,5 @@ export default function UserAccount() {
         </Paragraph>
       </Box>
 
-      <Button>Upgrade to Pro</Button>
     </FlexRowAlign>;
 }
--- src/spsda/layouts/layout-parts/navigation.jsx
+++ src/spsda/layouts/layout-parts/navigation.jsx
@@ -15,20 +15,49 @@ export const navigations = [
   {
     name: 'Analytics',
     path: '/analytics',
+    icon: duotone.Apps,
+  },
+  { // Admin
+    name: 'Teachers',
+    path: '/teachers',
     icon: duotone.PersonChalkboard,
   },
+  { // Admin
+    name: 'Subjects',
+    path: '/subjects',
+    icon: duotone.Folder,
+  },
+  { // Teacher
+    name: 'Students',
+    path: '/students',
+    icon: duotone.PersonCircleCheck,
+  },
+  { // Teacher
+    name: 'Class',
+    path: '/class',
+    icon: duotone.UserList,
+  },  
+  { // Teacher
+    name: 'Reports',
+    path: '/reports',
+    icon: duotone.Report,
+  },  
   {
     name: 'Maintenance',
     icon: duotone.TodoList,
     children: [
       {
-        name: 'CRM',
-        path: '/demo/dashboard/crm'
+        name: 'School Years',
+        path: '/school-years'
       },
       {
-        name: 'CRM 2',
-        path: '/demo/dashboard/crm-2'
-      }
+        name: 'Levels',
+        path: '/levels'
+      },
+      {
+        name: 'Sections',
+        path: '/sections'
+      },      
     ]
   }
 ];
